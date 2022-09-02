10b639f4b86c91959d4e2950690129c51862f1d5
ulyana_belousova <tentakly@list.ru>
2022-09-02 21:10:45 +0300

    задача 6

---
+++ task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6. Является ли число четным
+Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberN % 2 != 1)
+{
+   Console.WriteLine("число "+ numberN +" является четным"); 
+}
+else
+{
+    Console.WriteLine("число "+ numberN +" является нечетным");
+}
