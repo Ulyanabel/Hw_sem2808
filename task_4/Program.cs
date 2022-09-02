423a34a187140df2faa0fd180addafd636db0bc9
ulyana_belousova <tentakly@list.ru>
2022-09-02 20:31:49 +0300

    Задача 4

---
+++ task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4. Максимальное из трех чисел.
+Console.WriteLine("Введите три числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальное число из трех: " + max);
